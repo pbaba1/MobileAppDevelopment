b44760953de127661680d73f232cf5ddb522ee90
karthiks1997 <75327869+karthiks1997@users.noreply.github.com>
2022-04-10 23:41:26 -0400

    Created FAQ page

--- veershaivlingayat/lib/StaticScreens/contact-us.dart
+++ veershaivlingayat/lib/StaticScreens/contact-us.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:veershaivlingayat/Homepage/navdrawer.dart';
 import 'package:veershaivlingayat/utils/constants.dart' as c;
-import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactUs extends StatelessWidget {
@@ -25,145 +24,148 @@ class ContactUs extends StatelessWidget {
         title: const Text('Contact Us'),
         backgroundColor: Color(c.appColor),
       ),
-      body: Center(
-        child: Column(children: [
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
-              child: Text(
-                'Veershaiv Vivah Mandal, Pune',
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
-              )),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 20.0),
-              child: Text('Address:',
-                  textAlign: TextAlign.justify,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(children: [
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
+                child: Text(
+                  'Veershaiv Vivah Mandal, Pune',
                   style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                )),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 20.0),
+                child: Text('Address:',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                child: Text('Krishna Chember,Basement Shop No. 5,',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
                       fontSize: 14.0,
                       color: Colors.black,
-                      fontWeight: FontWeight.bold))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
-              child: Text('Krishna Chember,Basement Shop No. 5,',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
-              child: Text('Pune-Satara Road,Near Laxminarayan Theater/',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
-              child: Text('Near Central Bank of India, SWARGATE,',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
-              child: Text('Pune-411037,',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
-              child: Text('Maharashtra, India',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
-              child: Text('Contact No : 020-24260062 /24263879',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-              child: Text('Mobile:9371029775 /9420696689',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          Padding(
-              padding: const EdgeInsets.fromLTRB(15.0, 25.0, 15.0, 0.0),
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    const TextSpan(
-                        text: 'E-Mail: ',
+                    ))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: Text('Pune-Satara Road,Near Laxminarayan Theater/',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: Text('Near Central Bank of India, SWARGATE,',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: Text('Pune-411037,',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: Text('Maharashtra, India',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: Text('Contact No : 020-24260062 /24263879',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ))),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                child: Text('Mobile:9371029775 /9420696689',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ))),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(15.0, 25.0, 15.0, 0.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      const TextSpan(
+                          text: 'E-Mail: ',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.black,
+                          )),
+                      TextSpan(
+                        text: 'veershaiv@gmail.com',
+                        style: const TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            launch('mailto:veershaiv@gmail.com');
+                          },
+                      ),
+                    ],
+                  ),
+                )),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      const TextSpan(
+                          text: 'Website: ',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.black,
+                          )),
+                      TextSpan(
+                        text: 'www.veershaivlingayat.com',
+                        style: const TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            launch('www.veershaivlingayat.com');
+                          },
+                      ),
+                    ],
+                  ),
+                )),
+            const Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 25.0, 15.0, 0.0),
+                child:
+                    Text('Office Timings: 11:00 AM - 7:00 PM (Monday closed)',
+                        textAlign: TextAlign.justify,
                         style: TextStyle(
                           fontSize: 14.0,
                           color: Colors.black,
-                        )),
-                    TextSpan(
-                      text: 'veershaiv@gmail.com',
-                      style: const TextStyle(color: Colors.blue),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          launch('mailto:veershaiv@gmail.com');
-                        },
-                    ),
-                  ],
-                ),
-              )),
-          Padding(
-              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    const TextSpan(
-                        text: 'Website: ',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.black,
-                        )),
-                    TextSpan(
-                      text: 'www.veershaivlingayat.com',
-                      style: const TextStyle(color: Colors.blue),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          launch('www.veershaivlingayat.com');
-                        },
-                    ),
-                  ],
-                ),
-              )),
-          const Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 25.0, 15.0, 0.0),
-              child: Text('Office Timings: 11:00 AM - 7:00 PM (Monday closed)',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ))),
-          Padding(
-              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
-              child: GestureDetector(
-                onTap: _launchURL,
-                child: Image.asset(
-                  'assets/facebook_logo.png',
-                  width: 50.0,
-                  height: 50.0,
-                  fit: BoxFit.cover,
-                ),
-              )),
-        ]),
+                        ))),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: GestureDetector(
+                  onTap: _launchURL,
+                  child: Image.asset(
+                    'assets/facebook_logo.png',
+                    width: 50.0,
+                    height: 50.0,
+                    fit: BoxFit.cover,
+                  ),
+                )),
+          ]),
+        ),
       ),
     );
   }
